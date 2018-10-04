9f781b7c86407c762a125f0f2e1b33d67b38bb45
jonathans <jonathans@p-01-0k214-0016.schulen-su.de>
2018-10-04 08:28:07 +0200

    Hi i bims Jonny

---
+++ queue/src/main.java
@@ -0,0 +1,50 @@
+
+public class main {
+	
+	public static void main (String[] args)
+	{
+		
+	
+	
+	
+	
+	
+	Queue<Kunde> Snake = new Queue<Kunde>();
+	
+	
+	
+	
+	
+	Kunde Kunde1 = new Kunde("test");
+    Kunde Kunde2 = new Kunde("est2");
+	Kunde Kunde3 = new Kunde("test²");
+	Kunde Kunde4 = new Kunde("testst³");
+	Kunde Kunde5 = new Kunde("johnny");
+
+	/*Kunde1.setName("nake");
+	Kunde2.setName("make");
+	Kunde3.setName("lake");
+	Kunde4.setName("cake");
+	Kunde5.setName("kake");
+	
+	*/
+	
+	Snake.enqueue(Kunde1);
+	Snake.enqueue(Kunde2);
+	Snake.enqueue(Kunde3);
+	Snake.enqueue(Kunde4);
+	Snake.enqueue(Kunde5);
+	
+	
+	Snake.front();
+	for(int i = 0; i < 5; i++) {
+	System.out.println(Snake.front());
+	
+	Snake.dequeue();
+	
+	
+	
+	}
+	
+}
+}
