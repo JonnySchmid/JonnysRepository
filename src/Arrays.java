af3940e54b9204974b5eb646b0f0872b9329167f
jonathans <jonathans@p-01-0k214-0016.schulen-su.de>
2019-01-17 08:51:47 +0100

    tewst

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,20 +1,7 @@
-import javax.swing.*;
+
 public class Arrays {
 
-	int eins;
-	int zwei;
-	int drei;
 	
-	
-	int[] obenlinks   = {eins, zwei, drei};
-	int[] obenmitte   = {eins, zwei, drei};
-	int[] obenrechts  = {eins, zwei, drei};
-	int[] mittelinks  = {eins, zwei, drei};
-	int[] mittemitte  = {eins, zwei, drei};
-	int[] mitterechts = {eins, zwei, drei};
-	int[] untenlinks  = {eins, zwei, drei};
-	int[] untenmitte  = {eins, zwei, drei};
-	int[] untenrechts = {eins, zwei, drei};
 	
 	
 	public Arrays(){
@@ -23,5 +10,125 @@ public class Arrays {
 		 ImageIcon zwei = new ImageIcon("kreuz.jpg");
 		 ImageIcon drei = new ImageIcon("kreis.png");	
 		
+	}
+
+
+	public int[] getObenlinks() {
+		return obenlinks;
+	}
+
+
+	public void setObenlinks(int[] obenlinks) {
+		this.obenlinks = obenlinks;
+	}
+
+
+	public int getEins() {
+		return eins;
+	}
+
+
+	public void setEins(int eins) {
+		this.eins = eins;
+	}
+
+
+	public int getZwei() {
+		return zwei;
+	}
+
+
+	public void setZwei(int zwei) {
+		this.zwei = zwei;
+	}
+
+
+	public int getDrei() {
+		return drei;
+	}
+
+
+	public void setDrei(int drei) {
+		this.drei = drei;
+	}
+
+
+	public int[] getObenmitte() {
+		return obenmitte;
+	}
+
+
+	public void setObenmitte(int[] obenmitte) {
+		this.obenmitte = obenmitte;
+	}
+
+
+	public int[] getObenrechts() {
+		return obenrechts;
+	}
+
+
+	public void setObenrechts(int[] obenrechts) {
+		this.obenrechts = obenrechts;
+	}
+
+
+	public int[] getMittelinks() {
+		return mittelinks;
+	}
+
+
+	public void setMittelinks(int[] mittelinks) {
+		this.mittelinks = mittelinks;
+	}
+
+
+	public int[] getMittemitte() {
+		return mittemitte;
+	}
+
+
+	public void setMittemitte(int[] mittemitte) {
+		this.mittemitte = mittemitte;
+	}
+
+
+	public int[] getMitterechts() {
+		return mitterechts;
+	}
+
+
+	public void setMitterechts(int[] mitterechts) {
+		this.mitterechts = mitterechts;
+	}
+
+
+	public int[] getUntenlinks() {
+		return untenlinks;
+	}
+
+
+	public void setUntenlinks(int[] untenlinks) {
+		this.untenlinks = untenlinks;
+	}
+
+
+	public int[] getUntenmitte() {
+		return untenmitte;
+	}
+
+
+	public void setUntenmitte(int[] untenmitte) {
+		this.untenmitte = untenmitte;
+	}
+
+
+	public int[] getUntenrechts() {
+		return untenrechts;
+	}
+
+
+	public void setUntenrechts(int[] untenrechts) {
+		this.untenrechts = untenrechts;
 	}
 }
